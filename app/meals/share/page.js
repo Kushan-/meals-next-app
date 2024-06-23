a4ebb1bb6fa4b6127176487672f16ab612f658f3
kushan <kushan.joshi@outlook.com>
2024-06-22 21:08:23 -0400

    11: form Data for pendind form submission

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,11 +1,13 @@
-// 'use client'
+'use client'
+import { useFormState} from 'react-dom'
 
 import ImagePicker from '@/components/meals/image-picker';
+import MealsForumSubmit from '@/components/meals/meals-forum-submit';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-
+  const [state, formAction] = useFormState( shareMeal, {msg: null} );
   return (
     <>
       <header className={classes.header}>
@@ -15,7 +17,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -44,8 +46,10 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label= "Your image" name="image"/>
+          {state.msg && <p>{state.msg}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsForumSubmit/>
+            
           </p>
         </form>
       </main>
